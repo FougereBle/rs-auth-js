6accc016a121440148561992db4001ca53d823e6
Romain Simon <romain.simon.0608@gmail.com>
2021-02-02 10:25:16 +0100

    add error code

--- src/js-auth/auth/auth.js
+++ src/js-auth/auth/auth.js
@@ -9,12 +9,14 @@ const createError = (err) => {
     return {
       error: true,
       response: true,
+      code: err.response.status,
       message: err.response.data[config.errorVar],
     };
   } else {
     return {
       error: true,
       response: false,
+      code: 409,
       message: "Service Unavailable",
     };
   }
